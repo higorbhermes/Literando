e31af757245e59b1d5fa58203ff994afd68fbdd0
Higor Hermes <higorbhermes@gmail.com>
2017-07-07 12:49:19 -0300

    Intent e listView

--- app/src/main/java/higorhermes/literando/MainActivity.java
+++ app/src/main/java/higorhermes/literando/MainActivity.java
@@ -3,13 +3,22 @@ package higorhermes.literando;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Button ok = (Button) findViewById(R.id.button_entrar);
+            ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, TelaPrincipal.class);
+                    startActivity(intent);
+                }
+            });
+        }
 }
