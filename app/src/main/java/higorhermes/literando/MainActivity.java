92537e167b7aef9e15e134e29e0df700b4795baf
Higor Hermes <higorbhermes@gmail.com>
2017-07-07 12:20:27 -0300

    Tentativa de fazer intent e listView

--- app/src/main/java/higorhermes/literando/MainActivity.java
+++ app/src/main/java/higorhermes/literando/MainActivity.java
@@ -1,7 +1,9 @@
 package higorhermes.literando;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/higorhermes/literando/TelaPrincipal.java
+++ app/src/main/java/higorhermes/literando/TelaPrincipal.java
@@ -2,6 +2,8 @@ package higorhermes.literando;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TelaPrincipal extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class TelaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+
+        String itens[] = {"Novo Livro", "Meus Livros", "Buscar Livro"};
+        ListView listaSimples = (ListView) findViewById(R.id.lista);
+        ArrayAdapter<String> adapterSimples = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itens);
+
+        listaSimples.setAdapter(adapterSimples);
     }
 }
